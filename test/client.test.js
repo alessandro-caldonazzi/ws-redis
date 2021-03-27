095ab44a2b7cef350e0555d0d78ae7ca679dfb4a
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2021-03-27 21:59:57 +0100

    TEST: add test

--- test/client.test.js
+++ test/client.test.js
@@ -1,20 +1,82 @@
 const WsClient = require("../src/client/client");
 const WebSocket = require("ws");
+const wsRedis = require("../src/index");
+let connection;
+
+test("setIdentifier", () => {
+    try {
+        wsRedis.init(new WebSocket.Server({ port: 8080 }));
+
+        connection = new WsClient({
+            url: "ws://localhost:8080",
+            websocket: WebSocket,
+        });
+        connection.setAuthenticationToken("foo");
+        expect(connection.authenticationToken).toBe("foo");
+    } catch (error) {}
+});
+
+test("getTotMessages", () => {
+    try {
+        wsRedis.init(new WebSocket.Server({ port: 8080 }));
+
+        connection = new WsClient({
+            url: "ws://localhost:8080",
+            websocket: WebSocket,
+        });
+        expect(connection.getTotMessages()).toBe(0);
+    } catch (error) {}
+});
+
+test("send/receive on channel", (done) => {
+    try {
+        wsRedis.init(new WebSocket.Server({ port: 8080 }));
+
+        wsRedis.onConnection((ws, authenticationToken) => {
+            wsRedis.addUser("testUser", ws);
+            wsRedis.sendMessageToUser("testUser", "testChannel", "testMessage");
+        });
+
+        connection = new WsClient({
+            url: "ws://localhost:8080",
+            websocket: WebSocket,
+        });
+
+        connection.onMessage("testChannel", (message) => {
+            console.log("arrivato");
+            expect(message).toBe("testMessage");
+            done();
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+afterEach(() => {
+    wsRedis.close();
+    wsRedis.clean();
+    try {
+        connection.close();
+    } catch (error) {}
+});
 
 module.exports = {
     connect: () => {
         const connection = new WsClient({
             url: "ws://localhost:8080",
             websocket: WebSocket,
-            identifier: "clientIdentifier",
+            authenticationToken: "authenticationToken",
         });
+        setTimeout(() => connection.close(), 100);
     },
-    sendMessageToChannel: () => {
+    sendMessageToChannel: async () => {
         const connection = new WsClient({
             url: "ws://localhost:8080",
             websocket: WebSocket,
-            identifier: "clientIdentifier",
+            authenticationToken: "authenticationToken",
         });
-        connection.send("testChannel", "testMessage");
+        await connection.send("testChannel", "testMessage");
+        await connection.send("testChannel", "testMessage");
+        connection.close();
     },
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -4,12 +4,25 @@ const client = require("./client.test");
 
 test("authentication", (done) => {
     wsRedis.init(new WebSocket.Server({ port: 8080 }));
-    client.connect();
-    wsRedis.checkAuthentication((identifier) => {
-        //check if identifier is valid
-        expect(identifier).toBe("clientIdentifier");
+    client.sendMessageToChannel();
+    wsRedis.checkAuthentication((authenticationToken) => {
+        //check if authenticationToken is valid
+        expect(authenticationToken).toBe("authenticationToken");
         done();
         return true;
+    });
+    //required but not interested in this test
+    wsRedis.onConnection(() => {});
+});
+
+test("authentication fails", (done) => {
+    wsRedis.init(new WebSocket.Server({ port: 8080 }));
+    client.sendMessageToChannel();
+    wsRedis.checkAuthentication((authenticationToken) => {
+        //check if authenticationToken is valid
+        expect(authenticationToken).toBe("authenticationToken");
+        done();
+        return false;
     });
     //required but not interested in this test
     wsRedis.onConnection(() => {});
@@ -18,8 +31,8 @@ test("authentication", (done) => {
 test("onConnection", (done) => {
     wsRedis.init(new WebSocket.Server({ port: 8080 }));
     client.connect();
-    wsRedis.onConnection((ws, identifier) => {
-        expect(identifier).toBe("clientIdentifier");
+    wsRedis.onConnection((ws, authenticationToken) => {
+        expect(authenticationToken).toBe("authenticationToken");
         done();
     });
 });
@@ -34,6 +47,69 @@ test("receive message on channel", (done) => {
     });
     //required but not interested in this test
     wsRedis.onConnection(() => {});
+});
+
+test("provide no callback to onMessage", () => {
+    wsRedis.init(new WebSocket.Server({ port: 8080 }));
+    expect(() => {
+        wsRedis.onMessage("testChannel", "this should not work");
+    }).toThrow(Error);
+
+    //required but not interested in this test
+    wsRedis.onConnection(() => {});
+});
+
+test("provide same channel multiple times to onMessage", () => {
+    wsRedis.init(new WebSocket.Server({ port: 8080 }));
+    wsRedis.onMessage("testChannel", (data, ws) => {});
+
+    expect(() => {
+        wsRedis.onMessage("testChannel", (data, ws) => {});
+    }).toThrow(Error);
+
+    //required but not interested in this test
+    wsRedis.onConnection(() => {});
+});
+
+test("provide no callback to onConnection", () => {
+    wsRedis.init(new WebSocket.Server({ port: 8080 }));
+    expect(() => {
+        wsRedis.onConnection("this should not work");
+    }).toThrow(Error);
+});
+
+test("provide duplicate callback to onConnection", () => {
+    wsRedis.init(new WebSocket.Server({ port: 8080 }));
+    wsRedis.onConnection(() => {});
+    expect(() => {
+        wsRedis.onConnection(() => {});
+    }).toThrow(Error);
+});
+
+test("provide no callback to checkAuthentication", () => {
+    wsRedis.init(new WebSocket.Server({ port: 8080 }));
+    expect(() => {
+        wsRedis.checkAuthentication("this should not work");
+    }).toThrow(Error);
+});
+
+test("provide duplicate callback to checkAuthentication", () => {
+    wsRedis.init(new WebSocket.Server({ port: 8080 }));
+    wsRedis.checkAuthentication(() => {});
+    expect(() => {
+        wsRedis.checkAuthentication(() => {});
+    }).toThrow(Error);
+});
+
+test("provide invalid WebSocket instance to init", () => {
+    expect(() => {
+        wsRedis.init("this is not a valid WebSocket instance");
+    }).toThrow(Error);
+});
+
+test("provide invalid json to isJson", () => {
+    const isJson = require("../src/utils").isJson;
+    expect(isJson("this is not a valid Json")).toBe(false);
 });
 
 afterEach(() => {
