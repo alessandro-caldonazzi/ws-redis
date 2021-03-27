72fc1a6e34522ed205d97bee2b29f83d9c371968
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2021-03-27 16:02:33 +0100

    ADD: handling.js with handleMessage and onConnection

---
+++ src/handling.js
@@ -0,0 +1,36 @@
+let callbacks = {};
+let config = {};
+let users = {};
+let groups = {};
+
+async function handleMessage(json, ws) {
+    if (json?.token && config.checkAuthenticationCallback) {
+        const isAuthenticated = await config.checkAuthenticationCallback(
+            json.token
+        );
+        if (!isAuthenticated) return;
+    }
+    if (json?.channel in callbacks && json.data) {
+        callbacks[json.channel](
+            { identifier: json.token, message: json.data },
+            ws
+        );
+    }
+}
+
+function onConnection(callback) {
+    if (typeof callback !== "function")
+        throw new Error("Callback must be a function");
+    if (config.onConnectionCallback)
+        throw new Error("onConnectionCallback is already defined");
+    config.onConnectionCallback = callback;
+}
+
+module.exports = {
+    callbacks,
+    config,
+    users,
+    groups,
+    handleMessage,
+    onConnection,
+};
