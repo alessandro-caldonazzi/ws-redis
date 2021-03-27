faae6272803a19b54458ccba74368110a9472d2f
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2021-03-27 16:01:03 +0100

    UTILS: isJson (json validator), isInitial (check if is the first msg)

---
+++ src/utils.js
@@ -0,0 +1,17 @@
+function isJson(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+function isInitial(json) {
+    return json.isInitial == true;
+}
+
+module.exports = {
+    isJson,
+    isInitial,
+};
