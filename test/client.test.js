caa107cb2e7db9a407460246cac618ae2973093e
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2021-03-28 13:02:21 +0200

    TEST: add test in client.test.js and refactor

--- test/client.test.js
+++ test/client.test.js
@@ -4,52 +4,107 @@ const wsRedis = require("../src/index");
 let connection;
 
 test("setIdentifier", () => {
-    try {
-        wsRedis.init(new WebSocket.Server({ port: 8080 }));
+    wsRedis.init(new WebSocket.Server({ port: 8080 }));
 
-        connection = new WsClient({
-            url: "ws://localhost:8080",
-            websocket: WebSocket,
-        });
-        connection.setAuthenticationToken("foo");
-        expect(connection.authenticationToken).toBe("foo");
-    } catch (error) {}
+    connection = new WsClient({
+        url: "ws://localhost:8080",
+        websocket: WebSocket,
+    });
+    connection.setAuthenticationToken("foo");
+    expect(connection.authenticationToken).toBe("foo");
 });
 
 test("getTotMessages", () => {
-    try {
-        wsRedis.init(new WebSocket.Server({ port: 8080 }));
+    wsRedis.init(new WebSocket.Server({ port: 8080 }));
 
-        connection = new WsClient({
-            url: "ws://localhost:8080",
-            websocket: WebSocket,
-        });
-        expect(connection.getTotMessages()).toBe(0);
-    } catch (error) {}
+    connection = new WsClient({
+        url: "ws://localhost:8080",
+        websocket: WebSocket,
+    });
+    expect(connection.getTotMessages()).toBe(0);
 });
 
 test("send/receive on channel", (done) => {
-    try {
-        wsRedis.init(new WebSocket.Server({ port: 8080 }));
+    wsRedis.init(new WebSocket.Server({ port: 8080 }));
 
-        wsRedis.onConnection((ws, authenticationToken) => {
-            wsRedis.addUser("testUser", ws);
-            wsRedis.sendMessageToUser("testUser", "testChannel", "testMessage");
-        });
+    wsRedis.onConnection((ws, authenticationToken) => {
+        wsRedis.addUser("testUser", ws);
+        wsRedis.sendMessageToUser("testUser", "testChannel", "testMessage");
+    });
 
-        connection = new WsClient({
-            url: "ws://localhost:8080",
-            websocket: WebSocket,
-        });
+    connection = new WsClient({
+        url: "ws://localhost:8080",
+        websocket: WebSocket,
+    });
 
-        connection.onMessage("testChannel", (message) => {
-            console.log("arrivato");
-            expect(message).toBe("testMessage");
-            done();
-        });
-    } catch (error) {
-        console.log(error);
-    }
+    connection.onMessage("testChannel", (message) => {
+        console.log("arrivato");
+        expect(message).toBe("testMessage");
+        done();
+    });
+});
+
+test("provide invalid parameters in onMessage", () => {
+    wsRedis.init(new WebSocket.Server({ port: 8080 }));
+
+    connection = new WsClient({
+        url: "ws://localhost:8080",
+        websocket: WebSocket,
+    });
+    expect(() => {
+        connection.onMessage(123, (message) => {});
+    }).toThrow(Error);
+
+    expect(() => {
+        connection.onMessage("validChannelName", "invalidCallback");
+    }).toThrow(Error);
+
+    connection.onMessage("duplicateChannel", () => {});
+    expect(() => {
+        connection.onMessage("duplicateChannel", () => {});
+    }).toThrow(Error);
+});
+
+test("send/receive on group", (done) => {
+    wsRedis.init(new WebSocket.Server({ port: 8080 }));
+    let connectedUsers = 0,
+        arrivedMessages = 0;
+    wsRedis.onConnection((ws, authenticationToken) => {
+        connectedUsers++;
+        //add all connected user to testGroup
+        wsRedis.addToGroup("testGroup", ws);
+        if (connectedUsers === 2)
+            wsRedis.sendMessageToGroup(
+                "testGroup",
+                "testChannel",
+                "testMessage"
+            );
+    });
+
+    //connect two users
+    const connUser1 = new WsClient({
+        url: "ws://localhost:8080",
+        websocket: WebSocket,
+    });
+
+    const connUser2 = new WsClient({
+        url: "ws://localhost:8080",
+        websocket: WebSocket,
+    });
+
+    connUser1.onMessage("testChannel", (message) => {
+        expect(message).toBe("testMessage");
+        //after the last message is received, test is done
+        arrivedMessages++;
+        if (arrivedMessages === 2) done();
+    });
+
+    connUser2.onMessage("testChannel", (message) => {
+        expect(message).toBe("testMessage");
+        //after the last message is received, test is done
+        arrivedMessages++;
+        if (arrivedMessages === 2) done();
+    });
 });
 
 afterEach(() => {
