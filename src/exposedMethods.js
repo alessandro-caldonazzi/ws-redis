a007d6aa691cb8614f629eb05cdd614bab42bd9e
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2021-05-13 19:02:39 +0200

    DOCS: add docs to index.js and exposedMethod.js

--- src/exposedMethods.js
+++ src/exposedMethods.js
@@ -1,5 +1,19 @@
 let { callbacks, users, groups, config } = require("./handling");
 
+/**
+ * Callback where to receive message specific to a channel
+ * @callback onMessageCallback
+ * @param {Object} message - Json object containing the message and the authenticationToken
+ * @param {*} message.authenticationToken - The authenticationToken set on client side
+ * @param {*} message.data - Data sent by client
+ * @param {WebSocket} ws - Websocket client instance, useful if you want to add the user to a group
+ */
+
+/**
+ * Set the callback to call when the user receive a message
+ * @param {string} channel - Channel name to listen to
+ * @param {onMessageCallback} callback - Callback where to receive the message
+ */
 function onMessage(channel, callback) {
     if (typeof channel !== "string") throw new Error("Invalid channel name");
     if (typeof callback !== "function") throw new Error("Callback must be a function");
@@ -7,29 +21,69 @@ function onMessage(channel, callback) {
     callbacks[channel] = callback;
 }
 
+/**
+ * Remember a user identified by an identifier
+ * @param {string} identifier - User identifier
+ * @param {WebSocket} ws - Websocket client instance
+ */
 function addUser(identifier, ws) {
     users[identifier] = ws;
 }
 
+/**
+ * Add a user to a group
+ * @param {string} identifier - group identifier
+ * @param {WebSocket} ws - Websocket client instance
+ */
 function addToGroup(identifier, ws) {
     if (!groups[identifier]) groups[identifier] = [];
     groups[identifier].push(ws);
 }
 
+/**
+ * Delete a user by identifier
+ * @param {string} identifier - User identifier
+ */
 function deleteUser(identifier) {
     delete users[identifier];
 }
 
+/**
+ * Delete a group by identifier
+ * @param {string} identifier - Group identifier
+ */
 function deleteGroup(identifier) {
     delete groups[identifier];
 }
 
+/**
+ * Callback where to receive new user connection
+ * @callback onConnectionCallback
+ * @param {WebSocket} ws - Websocket client instance, useful if you want to add the user to a group
+ * @param {*} token - User authenticationToken set on client side
+ */
+
+/**
+ * Set the callback to call when a new user is connected
+ * @param {onConnectionCallback} callback - Callback where to receive new user connection
+ */
 function onConnection(callback) {
     if (typeof callback !== "function") throw new Error("Callback must be a function");
     if (config.onConnectionCallback) throw new Error("onConnectionCallback is already defined");
     config.onConnectionCallback = callback;
 }
 
+/**
+ * Function where you can validate the authenticationToken
+ * @callback checkAuthenticationCallback
+ * @param {*} token - User authenticationToken set on client side
+ * @return {boolean|Promise<boolean>} true means the token is valid
+ */
+
+/**
+ * Set the callback to validate authenticationToken
+ * @param {checkAuthenticationCallback} callback - Function where you can validate the authenticationToken
+ */
 function checkAuthentication(callback) {
     if (typeof callback !== "function") throw new Error("Callback must be a function");
     if (config.checkAuthenticationCallback)
@@ -37,6 +91,17 @@ function checkAuthentication(callback) {
     config.checkAuthenticationCallback = callback;
 }
 
+/**
+ * Callback where to be notified when user close the connection
+ * @callback onClientClosedCallback
+ * @param {string} userIdentifier - Identifier of the user
+ * @param {String[]} groups - List of groups in which the user was present
+ */
+
+/**
+ * Set the callback where to be notified when user close the connection
+ * @param {onClientClosedCallback} callback - Callback where to be notified when user close the connection
+ */
 function onClientClosed(callback) {
     if (typeof callback !== "function") throw new Error("Callback must be a function");
     config.onConnectionClosed = callback;
--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function setup() {
                 handling.handleMessage(message, ws);
             }
         });
-        ws.on("close", (aa) => {
+        ws.on("close", () => {
             handling.deleteUserByConnection(ws);
         });
         ws.on("error", (e) => {
@@ -36,6 +36,12 @@ function setup() {
     });
 }
 
+/**
+ * Send message to a specific user
+ * @param {string} identifier - Username / user identifier
+ * @param {string} channel - Channel name (client will receive a message on this channel)
+ * @param {*} data - Data to send
+ */
 function sendMessageToUser(identifier, channel, data) {
     if (identifier in handling.users) {
         handling.users[identifier].send(JSON.stringify({ data, channel }));
@@ -47,6 +53,13 @@ function sendMessageToUser(identifier, channel, data) {
     }
 }
 
+/**
+ * Send message to all users in the given group
+ * @param {string} identifier - Group name
+ * @param {string} channel - Channel name (clients will receive a message on this channel)
+ * @param {*} data - Data to send
+ * @param {boolean=} pubOnRedis - Whether to publish on redis to ensure delivery to all clients
+ */
 function sendMessageToGroup(identifier, channel, data, pubOnRedis = true) {
     if (identifier in handling.groups) {
         handling.groups[identifier].forEach((user) => {
@@ -61,42 +74,49 @@ function sendMessageToGroup(identifier, channel, data, pubOnRedis = true) {
     }
 }
 
+/**
+ * Initiate ws-redis
+ * @param {WebSocket} wsInstance - WebSocket server instance
+ * @return {Promise} Promise object rapresents the redis status
+ */
+async function init(wsInstance) {
+    return new Promise((resolve, reject) => {
+        if (!(wsInstance instanceof ws || wsInstance instanceof ws.Server)) {
+            return reject("wsInstance must be a WebSocket instance");
+        }
+        websocket = wsInstance;
+        setup();
+        if (!redisPublisher) redisPublisher = new Redis();
+        if (!redisSubscriber) {
+            redisSubscriber = new Redis();
+
+            redisSubscriber.on("message", (c, message) => {
+                //TODO: check if the message is valid
+                const { identifier, channel, data, isGroup, pid } = JSON.parse(message);
+                if (pid == process.pid) return;
+                if (isGroup && identifier in handling.groups) {
+                    sendMessageToGroup(identifier, channel, data, false);
+                } else if (identifier in handling.users) {
+                    sendMessageToUser(identifier, channel, data);
+                }
+            });
+            redisSubscriber.subscribe("ws-redis", (err) => {
+                if (err) reject(err);
+                else resolve();
+            });
+        } else {
+            resolve();
+        }
+
+        pingPongInterval = setInterval(() => {
+            handling.pingPong(websocket);
+        }, 2000);
+    });
+}
+
 module.exports = {
     ...exposedMethods,
-    init: (wsInstance) => {
-        return new Promise((resolve, reject) => {
-            if (!(wsInstance instanceof ws || wsInstance instanceof ws.Server)) {
-                return reject("wsInstance must be a WebSocket instance");
-            }
-            websocket = wsInstance;
-            setup();
-            if (!redisPublisher) redisPublisher = new Redis();
-            if (!redisSubscriber) {
-                redisSubscriber = new Redis();
-
-                redisSubscriber.on("message", (c, message) => {
-                    //TODO: check if the message is valid
-                    const { identifier, channel, data, isGroup, pid } = JSON.parse(message);
-                    if (pid == process.pid) return;
-                    if (isGroup && identifier in handling.groups) {
-                        sendMessageToGroup(identifier, channel, data, false);
-                    } else if (identifier in handling.users) {
-                        sendMessageToUser(identifier, channel, data);
-                    }
-                });
-                redisSubscriber.subscribe("ws-redis", (err) => {
-                    if (err) reject(err);
-                    else resolve();
-                });
-            } else {
-                resolve();
-            }
-
-            pingPongInterval = setInterval(() => {
-                handling.pingPong(websocket);
-            }, 2000);
-        });
-    },
+    init,
     close: async () => {
         await websocket.close();
         clearInterval(pingPongInterval);
